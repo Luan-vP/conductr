a604385b1553eb6107cff1d8a20a9d4f2046e8c9
Luan van Pletsen <luanvanpletsen@gmail.com>
2026-05-10 17:32:02 +0100

    Swap treble clef for bass clef in CLI banner

--- crates/conductr/src/main.rs
+++ crates/conductr/src/main.rs
@@ -11,37 +11,21 @@ use conductr_tasks::beads::Beads;
 use serde::Serialize;
 
 const BANNER: &str = r#"
-       =IY;
-      XRVRRi
-     =Ri :RR
-     VR  ;RR
-     RY  tRY
-     RX ;RR;
-     tR=RRR
-     ;RRRR;
-     ;RRRi
-    ;RRRV
-   ;RRRRR:
-  :RRRV.RX
-  VRRR: ;R=
- =RRR+   YRitti;.
- VRRR  :YRRRRRRRRt.
-.RRRY ;RRYYR;;iRRRR.
-:RRRi RR; .Rt  .XRRY
-:RRR= RV   +R.  .RRR
-:RRRt tR    Rt   IRR.
- RRRR  ;;   +R.  XRY
- ;RRRt      .R+ +RR:
-  ;RRRX;     YRIRX:
-   .+RRRRIiitRRY;
-      :;+ii+;tR:
-             ;R:
-             iR
-    ;VRRi    Vi
-    RRRRR.  tR.
-    YRRR+ ;YV:
-    .tRRRRR+
-      .;;:
+        ..,,,,,..
+    .,;;;;;;;;;;;;;;,.
+  ,;;;'            `;;;;,       ,,
+ ,;'                 ';;;;,    ;;;;
+.;.;;;;,               ;;;;;.   ''
+;;;;;;;;                ;;;;;
+`;;;;;;'                ;;;;;
+                        ;;;;'   ,,
+                      .;;;;'   ;;;;
+                     ,;;;'      ''
+                   ,;;;'
+                ,;;;'
+            .;;;;'
+        .,;;;''
+    .,;;''
 
                      |\                         __3__          |
 ____|\_______________|\\_______________|_______'__|__`___|_____|___|__________
